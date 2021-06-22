2bdeeb73cc4e1f52e8715a73d59c11b77bda615f
AjayNaruka <naruka.ajay1997@gmail.com>
2021-06-22 17:15:55 +0200

    fin

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -7,7 +7,7 @@
   <link rel="stylesheet" href="{{ asset('css/app.css')}}">
   <link rel="preconnect" href="https://fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  
   
   <title>Document</title>
 </head>
